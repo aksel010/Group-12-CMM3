6ca1ecdd11fa5e80a0f3dd71700ed5141498aa80
Gregory Morgan <s2297115@sms.ed.ac.uk>
2025-11-15 20:37:34 +0000

    Fix m_dot_c assignment to use steady state values

--- Optimum_Current.py
+++ Optimum_Current.py
@@ -19,7 +19,7 @@ def I_params(I):
         DC_IR *24,      # R_cell * n _cell it passes through[Ω]
         A_s,     # A_s [m²]
         T_in,     # T_c_in [K]
-        m_dot_ss     # m_dot_c [kg/s]
+        get_steady_state_values()[0]     # m_dot_c [kg/s]
     )
 
 # Generate data points
