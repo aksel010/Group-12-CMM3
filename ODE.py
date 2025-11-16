1d716970ee16a0411ae0867e08ca8d511f995bcf
Nicholas <newtyj4444@gmail.com>
2025-11-16 15:06:51 +0000

    Faster ODE solver

--- ODE.py
+++ ODE.py
@@ -46,53 +46,39 @@ def dTb_dt (Tb, t, params):
 def Tb(dTdt, params,stepsize):
     # initial conditions
     t0 = 0
-    T0 = T_in
+    T0 = T_in  # still using the external variable
+
     # total solution interval
-    t_final= q_b / params[2]  # total time [s]
-    # step size
-    # Fourth Order Runge-Kutta method
+    t_final = q_b / params[2]
 
     # number of steps
-    n_step = math.ceil(t_final/stepsize)
+    n_step = math.ceil(t_final / stepsize)
 
-    # Definition of arrays to store the solution
-    T_rk = np.zeros(n_step+1)
-    t_rk = np.zeros(n_step+1)
-
-    # Initialize first element of solution arrays 
-    # with initial condition
+    # allocate solution arrays
+    T_rk = np.zeros(n_step + 1)
+    t_rk = np.zeros(n_step + 1)
     T_rk[0] = T0
-    t_rk[0] = t0 
+    t_rk[0] = t0
 
-    # Populate the x array
+    # populate the time array in one vectorized step
+    t_rk[1:] = np.arange(1, n_step + 1) * stepsize
+
+    # apply RK4
     for i in range(n_step):
-        t_rk[i+1]  = t_rk[i]  + stepsize
+        t_i = t_rk[i]
+        T_i = T_rk[i]
 
-    # Apply RK method n_step times
-    for i in range(n_step):
-    
-        # Compute the four slopes
-        t_dummy = t_rk[i]
-        T_dummy = T_rk[i]
-        k1 =  dTdt(T_dummy,t_dummy, params)
-        
-        t_dummy = t_rk[i]+stepsize/2
-        T_dummy = T_rk[i] + k1 * stepsize/2
-        k2 =  dTdt(T_dummy,t_dummy, params)
+        # compute all slopes in a compact way
+        k1 = dTdt(T_i, t_i, params)
+        k2 = dTdt(T_i + 0.5 * stepsize * k1, t_i + 0.5 * stepsize, params)
+        k3 = dTdt(T_i + 0.5 * stepsize * k2, t_i + 0.5 * stepsize, params)
+        k4 = dTdt(T_i + stepsize * k3, t_i + stepsize, params)
 
-        t_dummy = t_rk[i]+stepsize/2
-        T_dummy = T_rk[i] + k2 * stepsize/2
-        k3 =  dTdt(T_dummy,t_dummy, params)
+        # weighted average of slopes
+        slope = (k1 + 2*k2 + 2*k3 + k4) / 6
 
-        t_dummy = t_rk[i]+stepsize
-        T_dummy = T_rk[i] + k3 * stepsize
-        k4 =  dTdt(T_dummy,t_dummy, params)
-
-        # compute the slope as weighted average of four slopes
-        slope = 1/6 * k1 + 2/6 * k2 + 2/6 * k3 + 1/6 * k4 
-
-        # use the RK method
-        T_rk[i+1] = T_rk[i] + stepsize * slope  
+        # update solution
+        T_rk[i + 1] = T_i + stepsize * slope
 
     return t_rk, T_rk
     # ------------------------------------------------------
