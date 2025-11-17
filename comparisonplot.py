0f6f9771c2fe5dfe127f747ae173f088f5dd9b69
aksel010 <s2339702@ed.ac.uk>
2025-11-17 15:39:42 +0000

    I_0

--- comparisonplot.py
+++ comparisonplot.py
@@ -12,7 +12,6 @@ temp_num = dfnum['Temperature (K)'] - 273.15  # convert
 plt.plot(time_exp, temp_exp, label='Liu Et.Al', color='red', marker='o', linestyle='')
 plt.plot(time_num, temp_num, label='RK4 Model', color='blue', linestyle='-')
 
-plt.xlim(0, 50)
 plt.xlabel('Time (minutes)')          
 plt.ylabel('Temperature (°C)') 
 plt.title('Comparison of Model and Experimental')
