2989287c7b7f77d77cbb383fa6b5dbe0f5c8a021
Gregory Morgan <s2297115@sms.ed.ac.uk>
2025-11-18 00:48:34 +0000

    PEP8/PEP257: clear_val.py updated for naming, namespace safety, and docstring compliance. Committed for annotation branch review.

--- clear_val.py
+++ clear_val.py
@@ -1,7 +1,17 @@
-import pandas as pd
-def clearvars():    
-    for el in sorted(globals()):
+"""
+clear_val.py
+Helper utility to clear user-defined variables from global namespace.
+"""
+
+def clear_vars():
+    """
+    Delete all user-defined global variables (for interactive use).
+    Prints each variable name as it is deleted.
+    """
+    for el in sorted(list(globals().keys())):
         if '__' not in el:
-                print(f'deleted: {el}')
-                del el
-clearvars()
+            print(f'deleted: {el}')
+            del globals()[el]
+
+if __name__ == '__main__':
+    clear_vars()
